28df222bc508145c6244367d53c4a0845e005147
Gavrilajava <georgy.gavrilchik@gmail.com>
2023-08-06 17:41:02 -0500

    tasl-12

---
+++ task-12/cmd/server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+
+	"go-gavrila/task-12/pkg/crawler/spider"
+	"go-gavrila/task-12/pkg/index"
+	"go-gavrila/task-12/pkg/webapp"
+)
+
+const depth = 2
+
+var urls = [2]string{"https://go.dev", "https://golang.org"}
+
+func main() {
+
+	index, err := index.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if index.Empty() {
+		s := spider.New()
+		for _, url := range urls {
+			res, err := s.Scan(url, depth)
+			if err != nil {
+				log.Fatal(err)
+			}
+			index.Add(res)
+		}
+		if err = index.Save(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	webapp.Start(":8000", *index)
+
+}
