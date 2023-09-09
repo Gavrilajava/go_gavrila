e32c64010659e5b7d9774376bc3c45bde6ba7c66
Gavrilajava <georgy.gavrilchik@gmail.com>
2023-09-09 16:50:15 -0500

    task-18a

--- task-18/pkg/api/api.go
+++ task-18/pkg/api/api.go
@@ -54,7 +54,7 @@ func (a *API) getUrl(w http.ResponseWriter, r *http.Request) {
 func (a *API) addUrl(w http.ResponseWriter, r *http.Request) {
 	params := &Params{}
 
-	err := json.NewDecoder(r.Body).Decode(&params)
+	err := json.NewDecoder(r.Body).Decode(params)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
